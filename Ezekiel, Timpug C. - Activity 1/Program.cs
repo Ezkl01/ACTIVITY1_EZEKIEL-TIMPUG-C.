2208feda0365718e9986bfae7adcec1f791e6863
Ezkl01 <etimpug@csmbataan.ph.education>
2025-07-17 18:23:13 +0800

    average - solving problem number

---
+++ Ezekiel, Timpug C. - Activity 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Wellcome to My First Program");
+
+Console.WriteLine();
+    
+        Console.Write("integer  :  ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("decimal  :  ");
+        float num2 = Convert.ToSingle(Console.ReadLine());
+
+        Console.Write("double   :  ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+
+        double average = (num1 + num2 + num3) / 3;
+        Console.WriteLine("Average: " + average );
+
